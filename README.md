b5f18f1155415cea81949313edf39d038f19334a
Stefan Kaufhold <cere@fastmail.fm>
2016-06-16 04:18:37 +0200

    include both minified and full version in build

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # ES6 Module Base
 
 Boilerplate for an ES6 based module that is bundled with webpack.
+
+`npm run watch` to start webpack in watch mode an automatically recompile on changes
+
+`npm run build` to build the production bundle, which includes a full version and a minified version
+of the code.
+
+The build will be done automatically before the module is packaged and published by NPM.
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const WebpackNotifierPlugin = require('webpack-notifier');
+const webpack = require('webpack');
+
+const indexFile = './src/index.js';
 
 let config = {
-  entry: './src/index.js',
+  entry: {
+    bundle: indexFile,
+  },
   output: {
     path: __dirname,
-    filename: 'dist/bundle.js',
+    filename: 'dist/[name].js',
   },
   module: {
     loaders: [
@@ -15,9 +20,19 @@ let config = {
 
 if (process.env.npm_lifecycle_event === 'build') {
   config = Object.assign(config, {
+    entry: {
+      bundle: indexFile,
+      'bundle.min': indexFile,
+    },
     eslint: {
       failOnError: true,
     },
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        include: /\.min\.js$/,
+        minimize: true,
+      }),
+    ],
   });
 } else {
   config = Object.assign(config, {
